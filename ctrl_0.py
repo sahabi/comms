b615a9706db3d7bacdf629d19a3a7d4e10c13b35
Mohammed Alshiekh <sahabi@gmail.com>
2018-03-19 18:42:04 -0600

    added the generated controllers

--- ctrl_0.py
+++ ctrl_0.py
@@ -4,73 +4,83 @@ class Controller_0:
         self.state = 0
 
     def comms(self, tp):
-        
+
         if self.state == 1 and tp == 1:
-            return 3
-        
+            return 0
+
         if self.state == 0 and tp == 1:
             return 0
-        
+
+        if self.state == 3 and tp == 1:
+            return 1
+
+        if self.state == 2 and tp == 1:
+            return 1
+
 
     def move(self, i, c):
-        
-        if self.state == 0 and i == 2 and c == 0:
+
+        if self.state == 3 and i == 1 and c == 1:
             self.state = 1
-            return 0
-        
-        if self.state == 0 and i == 3 and c == 1:
+            return 3
+
+        if self.state == 0 and i == 1 and c == 1:
+            self.state = 2
+            return 3
+
+        if self.state == 3 and i == 0 and c == 1:
             self.state = 1
-            return 0
-        
-        if self.state == 0 and i == 2 and c == 1:
+            return 3
+
+        if self.state == 1 and i == 0 and c == 0:
             self.state = 0
-            return 1
-        
+            return 2
+
         if self.state == 1 and i == 0 and c == 1:
             self.state = 0
-            return 1
-        
+            return 2
+
         if self.state == 0 and i == 0 and c == 0:
             self.state = 1
-            return 0
-        
-        if self.state == 1 and i == 2 and c == 0:
+            return 3
+
+        if self.state == 3 and i == 1 and c == 0:
             self.state = 1
-            return 0
-        
-        if self.state == 0 and i == 3 and c == 0:
-            self.state = 1
-            return 0
-        
-        if self.state == 0 and i == 1 and c == 0:
+            return 3
+
+        if self.state == 2 and i == 1 and c == 1:
             self.state = 0
-            return 1
-        
-        if self.state == 1 and i == 0 and c == 0:
-            self.state = 1
-            return 0
-        
-        if self.state == 0 and i == 1 and c == 1:
+            return 2
+
+        if self.state == 2 and i == 0 and c == 1:
             self.state = 0
-            return 1
-        
-        if self.state == 0 and i == 0 and c == 1:
-            self.state = 0
-            return 1
-        
+            return 2
+
         if self.state == 1 and i == 1 and c == 0:
+            self.state = 3
+            return 2
+
+        if self.state == 2 and i == 0 and c == 0:
             self.state = 0
-            return 1
-        
+            return 2
+
+        if self.state == 0 and i == 0 and c == 1:
+            self.state = 1
+            return 3
+
+        if self.state == 3 and i == 0 and c == 0:
+            self.state = 1
+            return 3
+
+        if self.state == 0 and i == 1 and c == 0:
+            self.state = 2
+            return 3
+
+        if self.state == 2 and i == 1 and c == 0:
+            self.state = 0
+            return 2
+
         if self.state == 1 and i == 1 and c == 1:
-            self.state = 0
-            return 1
-        
-        if self.state == 1 and i == 3 and c == 0:
-            self.state = 1
-            return 0
-        
-        if self.state == 1 and i == 2 and c == 1:
-            self.state = 1
-            return 0
-        
+            self.state = 3
+            return 2
+
--- ctrl_1.py
+++ ctrl_1.py
@@ -4,112 +4,51 @@ class Controller_1:
         self.state = 0
 
     def comms(self, tp):
-        
+
+        if self.state == 3 and tp == 0:
+            return 0
+
         if self.state == 0 and tp == 0:
-            return 1
-        
+            return 0
+
         if self.state == 2 and tp == 0:
-            return 1
-        
+            return 0
+
         if self.state == 1 and tp == 0:
             return 0
-        
+
 
     def move(self, i, c):
-        
-        if self.state == 0 and i == 1 and c == 1:
+
+        if self.state == 0 and i == None and c == 1:
             self.state = 2
-            return 2
-        
-        if self.state == 1 and i == 0 and c == 0:
-            self.state = 1
             return 3
-        
-        if self.state == 0 and i == 0 and c == 1:
-            self.state = 0
-            return 0
-        
-        if self.state == 2 and i == 1 and c == 0:
+
+        if self.state == 2 and i == None and c == 1:
             self.state = 1
-            return 3
-        
-        if self.state == 1 and i == 0 and c == 1:
-            self.state = 2
-            return 2
-        
-        if self.state == 1 and i == 1 and c == 2:
+            return 1
+
+        if self.state == 1 and i == None and c == 0:
+            self.state = 0
+            return 0
+
+        if self.state == 0 and i == None and c == 0:
             self.state = 1
+            return 1
+
+        if self.state == 1 and i == None and c == 1:
+            self.state = 3
             return 3
-        
-        if self.state == 0 and i == 0 and c == 2:
+
+        if self.state == 2 and i == None and c == 0:
             self.state = 1
-            return 3
-        
-        if self.state == 2 and i == 1 and c == 1:
-            self.state = 2
-            return 2
-        
-        if self.state == 2 and i == 0 and c == 3:
-            self.state = 2
-            return 2
-        
-        if self.state == 0 and i == 1 and c == 3:
+            return 1
+
+        if self.state == 3 and i == None and c == 0:
             self.state = 0
             return 0
-        
-        if self.state == 0 and i == 0 and c == 0:
-            self.state = 1
-            return 3
-        
-        if self.state == 2 and i == 0 and c == 2:
-            self.state = 2
-            return 2
-        
-        if self.state == 0 and i == 1 and c == 0:
+
+        if self.state == 3 and i == None and c == 1:
             self.state = 0
             return 0
-        
-        if self.state == 1 and i == 0 and c == 3:
-            self.state = 0
-            return 0
-        
-        if self.state == 2 and i == 1 and c == 3:
-            self.state = 1
-            return 3
-        
-        if self.state == 1 and i == 1 and c == 1:
-            self.state = 0
-            return 0
-        
-        if self.state == 2 and i == 0 and c == 1:
-            self.state = 2
-            return 2
-        
-        if self.state == 0 and i == 0 and c == 3:
-            self.state = 0
-            return 0
-        
-        if self.state == 1 and i == 1 and c == 0:
-            self.state = 0
-            return 0
-        
-        if self.state == 2 and i == 0 and c == 0:
-            self.state = 1
-            return 3
-        
-        if self.state == 0 and i == 1 and c == 2:
-            self.state = 1
-            return 3
-        
-        if self.state == 1 and i == 1 and c == 3:
-            self.state = 0
-            return 0
-        
-        if self.state == 2 and i == 1 and c == 2:
-            self.state = 0
-            return 0
-        
-        if self.state == 1 and i == 0 and c == 2:
-            self.state = 2
-            return 2
-        
+
